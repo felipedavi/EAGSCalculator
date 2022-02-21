179e107dbb1888761141637ee6d96f050ad5a112
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-02-21 10:02:25 -0300

    ViewBinding MainActivity.kt

--- app/src/main/java/com/ignorelist/krosky/eags_calculator/MainActivity.kt
+++ app/src/main/java/com/ignorelist/krosky/eags_calculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ignorelist.krosky.eags_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ignorelist.krosky.eags_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
