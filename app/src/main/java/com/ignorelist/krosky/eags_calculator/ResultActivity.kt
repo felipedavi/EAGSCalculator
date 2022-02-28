b08a0a39ffc16c3a86089b9ae3d6dc7d86a2ca27
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-02-28 18:59:16 -0300

    Tirando Código Inútil da ResultActivity.kt

--- app/src/main/java/com/ignorelist/krosky/eags_calculator/ResultActivity.kt
+++ app/src/main/java/com/ignorelist/krosky/eags_calculator/ResultActivity.kt
@@ -1,26 +1,16 @@
 package com.ignorelist.krosky.eags_calculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ignorelist.krosky.eags_calculator.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
-    @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val bundle :Bundle ?=intent.extras
-        val pt = bundle!!.getInt("pt")
-        val esp = bundle.getInt("esp")
-        if ((pt>=20) and (esp>=30)) {
-            val m = (((pt/40) + (2*(esp/60)))/3)*10
-            binding.textResult.text = "Média: $m"
-        } else {
-            binding.textResult.setText(R.string.fail)
-        }
     }
 }
