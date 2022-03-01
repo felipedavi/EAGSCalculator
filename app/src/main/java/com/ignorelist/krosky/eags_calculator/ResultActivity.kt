b127b47f1f0dfcfa6781eaba6f2903217f179a2c
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-03-01 01:12:10 -0300

    Fix da Fix ResultActivity.kt

--- app/src/main/java/com/ignorelist/krosky/eags_calculator/ResultActivity.kt
+++ app/src/main/java/com/ignorelist/krosky/eags_calculator/ResultActivity.kt
@@ -18,7 +18,7 @@ class ResultActivity : AppCompatActivity() {
         val mesp = (esp.toDouble()/60)*10
         val r = String.format("Média de Português: %.2f\nMédia das Específicas: %.2f", mp, mesp)
         if ((mp>=5) or (mesp>=5)) {
-            val m = mp+(mesp*2)/3
+            val m = (mp+(mesp*2))/3
             binding.textResult.text = String.format("%s\nMédia: %.2f", r,m)
         } else {
             binding.textResult.text = String.format("%s\n\nDesclassificado")
